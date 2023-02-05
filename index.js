95f8e5630f9ff450fc8835b70a9786689c018886
LaurentArcos <laurent.arcos@gmail.com>
2023-02-05 14:42:54 +0100

    réinitialisation projet

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-function faireQuelqueChose() {
-  console.log ('je fais un truc');
-  console.log (5 + 6);
-}
 
-faireQuelqueChose();
-
-console.log('hello');
-
-const arbre = 'pin parasol';
-
-console.log(arbre);
-
-const addition = (a, b) => {
-  console.log( a + b);
-};
-
-addition(4, 3);
-addition(432, 658);
