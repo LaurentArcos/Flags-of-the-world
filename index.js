00281b18f93c93b2529d67ac9cd94375b6eab1a3
LaurentArcos <laurent.arcos@gmail.com>
2023-02-05 14:14:50 +0100

    test de l'appel de la fonction

---
+++ index.js
@@ -0,0 +1,12 @@
+function faireQuelqueChose() {
+  console.log ('je fais un truc');
+  console.log (5 + 6);
+}
+
+faireQuelqueChose();
+
+console.log('hello');
+
+const arbre = 'pin parasol';
+
+console.log(arbre);
