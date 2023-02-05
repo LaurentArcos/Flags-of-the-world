e5d7a7d4e85979808c47602e51910ac61ef1e9d9
LaurentArcos <laurent.arcos@gmail.com>
2023-02-05 18:19:05 +0100

    filtres opérationnels

--- flags-of-the-world/src/components/Countries.js
+++ flags-of-the-world/src/components/Countries.js
@@ -6,7 +6,8 @@ const Countries = () => {
 
   const [data, setData]= useState([]);
   const [rangeValue, setRangeValue]= useState(36);
-  const radios = ["Afrique", "Amérique", "Asie", "Europe", "Océanie"];
+  const [selectedRadio, setSelectedRadio]= useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   useEffect (() =>{
     axios
@@ -25,16 +26,25 @@ const Countries = () => {
         onChange={(event) => setRangeValue(event.target.value)} />
         {radios.map((continent) =>(
           <li>
-            <input type="radio" id={continent} name="continentRadio" />
+            <input 
+            type="radio" 
+            id={continent} 
+            name="continentRadio"
+            checked={continent === selectedRadio}
+            onChange={(e) => setSelectedRadio(e.target.id)}
+            />
             <label htmlFor={continent}>{continent}</label>
           </li>
         ))}
       </ul>
+      {selectedRadio && <button onClick={() => setSelectedRadio("")}>Annuler la recherche</button>}
       <ul>
         {data
-        .slice(0, rangeValue)
-        .map((country, index) => (
-          <Card key={index} country={country}/>
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country}/>
           ))}
       </ul>
     </div>
