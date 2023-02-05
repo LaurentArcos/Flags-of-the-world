8d94768827dc8638f49ce0c24f54a130271d2e11
LaurentArcos <laurent.arcos@gmail.com>
2023-02-05 18:44:34 +0100

    name and size of logo changed

--- flags-of-the-world/src/components/Logo.js
+++ flags-of-the-world/src/components/Logo.js
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <div className="logo">
       <img src="./logo192.png" alt="logo react" />
-      <h3>React World</h3>
+      <h3>Flags of the world</h3>
     </div>
   );
 };
