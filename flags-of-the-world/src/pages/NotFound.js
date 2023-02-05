e74514c9d7e7a851ea708c6713d8ce0bea4b94e3
LaurentArcos <laurent.arcos@gmail.com>
2023-02-05 16:57:23 +0100

    Logo ok

--- flags-of-the-world/src/pages/NotFound.js
+++ flags-of-the-world/src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 
 const NotFound = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <h1>Erreur page 404</h1>
     </div>
